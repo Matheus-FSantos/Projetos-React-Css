5bdc9de2435fd11517b5d178fb5e5f747ba765a4
Matheus-FSantos <matheus.fs.contato@gmail.com>
2022-09-26 15:41:20 -0300

    Projeto 01 - ReactJS e CSS

--- Projeto 01 (com useState)/src/Paginas/Error/error.js
+++ Projeto 01 (com useState)/src/Paginas/Error/error.js
@@ -1,8 +1,12 @@
-import {Link} from 'react-router-dom';
+import './erro.css';
+
 function Erro(){
     return(
-        <div>
+        <div class='conteinerPrincipal'>
             <h1 align='center'>Erro 404</h1>
+            <hr />
+            <img width='200px' src="https://thumbs.dreamstime.com/b/reparo-quebrado-do-rob-81326219.jpg" />
+            <br/>
             <p>Essa pagina nao existe, volte para alguma dessas paginas acima</p>
         </div>
     );
